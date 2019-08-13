7ebf93c94908160332c94984268252b9ba43af68
anjanaft <anjanaft13@gmail.com>
2019-08-13 14:58:47 +0530

    Inbuilt modules

---
+++ node/operation.js
@@ -0,0 +1,23 @@
+module.exports.add=(a,b)=>
+{
+    var c=a+b;
+    return c;
+}
+
+module.exports.sub=(a,b)=>
+{
+    var c=a-b;
+    return c;
+}
+
+module.exports.mult=(a,b)=>
+{
+    var c=a*b;
+    return c;
+}
+
+module.exports.div=(a,b)=>
+{
+    var c=a/b;
+    return c;
+}
---
+++ node/sub.js
@@ -0,0 +1,5 @@
+const op=require('./operation');
+var a=2;
+var b=5;
+var c=op.sub(a,b);
+console.log(c);
